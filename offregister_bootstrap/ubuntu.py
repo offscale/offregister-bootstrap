77b97d87c8eb351fa8ee8eea176c19ad62e54659
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-16 21:32:17 -0400

    [offregister_bootstrap/ubuntu.py] Update to new threadsafe Python 3 compatible Fabric

--- offregister_bootstrap/ubuntu.py
+++ offregister_bootstrap/ubuntu.py
@@ -6,15 +6,15 @@ from offregister_fab_utils.ubuntu.hostname import set_hostname
 
 
 @require_os_version(16.04, op=gte)
-def set_hostname0(cache, *args, **kwargs):
+def set_hostname0(c, cache, *args, **kwargs):
     hostname = c.run("hostname", hide=True)
     if "." in hostname:
-        set_hostname(hostname.partition(".")[0])
+        set_hostname(c, hostname.partition(".")[0])
     return {"os_version": cache["os_version"]}
 
 
 @require_os_version(16.04, op=gte)
-def motd1(*args, **kwargs):
+def motd1(c, *args, **kwargs):
     if c.run("grep -Fqzw 18.10 /etc/lsb-release", warn=True, hide=True).exited != 0:
         apt_depends(c, "landscape-common")
 
