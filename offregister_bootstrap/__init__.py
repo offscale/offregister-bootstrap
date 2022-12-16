a927d0dce4f4f798ecbb42f78e9e22c0bf7db1ec
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-12-15 20:06:57 -0500

    [setup.py] Add missing imports (conforming to offscale 'standard' along the way)

--- offregister_bootstrap/__init__.py
+++ offregister_bootstrap/__init__.py
@@ -2,3 +2,4 @@
 
 __author__ = "Samuel Marks"
 __version__ = "0.0.1"
+__description__ = "Bootstrap deployment module for Fabric (offregister)"
--- setup.py
+++ setup.py
@@ -1,62 +1,102 @@
-from ast import parse
-from distutils.sysconfig import get_python_lib
-from functools import partial
-from os import listdir, path
+# -*- coding: utf-8 -*-
+
+"""
+setup.py implementation, interesting because it parsed the first __init__.py and
+    extracts the `__author__` and `__version__`
+"""
+
+from ast import Assign, Str, parse
+from operator import attrgetter
+from os import path
+from os.path import extsep
+from sys import version_info
 
 from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    package_name = "offregister_bootstrap"
+if version_info[0] == 2:
+    from itertools import ifilter as filter
+    from itertools import imap as map
 
-    with open(path.join(package_name, "__init__.py")) as f:
-        __author__, __version__ = map(
-            lambda const: const.value if hasattr(const, "value") else const.s,
+if version_info[:2] > (3, 7):
+    from ast import Constant
+else:
+    from ast import expr
+
+    # Constant. Will never be used in Python =< 3.8
+    Constant = type("Constant", (expr,), {})
+
+
+package_name = "offregister_bootstrap"
+
+with open(
+    path.join(path.dirname(__file__), "README{extsep}md".format(extsep=extsep)),
+    "rt",
+) as fh:
+    long_description = fh.read()
+
+
+def main():
+    """Main function for setup.py; this actually does the installation"""
+    with open(
+        path.join(
+            path.abspath(path.dirname(__file__)),
+            package_name,
+            "__init__{extsep}py".format(extsep=extsep),
+        )
+    ) as f:
+        parsed_init = parse(f.read())
+
+    __author__, __version__, __description__ = map(
+        lambda node: node.value if isinstance(node, Constant) else node.s,
+        filter(
+            lambda node: isinstance(node, (Constant, Str)),
             map(
                 attrgetter("value"),
-                map(
-                    itemgetter(0),
-                    map(
-                        attrgetter("body"),
-                        map(
-                            parse,
-                            filter(
-                                lambda line: line.startswith("__version__")
-                                or line.startswith("__author__"),
-                                f,
-                            ),
-                        ),
-                    ),
-                ),
+                filter(lambda node: isinstance(node, Assign), parsed_init.body),
             ),
-        )
-
-    to_funcs = lambda *paths: (
-        partial(path.join, path.dirname(__file__), package_name, *paths),
-        partial(path.join, get_python_lib(prefix=""), package_name, *paths),
+        ),
     )
-
-    _data_join, _data_install_dir = to_funcs("_data")
 
     setup(
         name=package_name,
         author=__author__,
+        author_email="807580+SamuelMarks@users.noreply.github.com",
         version=__version__,
-        description="Bootstrap deployment module for Fabric (offregister)",
+        description=__description__,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        install_requires=["six", "jsonref"],
+        test_suite="{}.tests".format(package_name),
+        packages=find_packages(),
+        package_dir={package_name: package_name},
         classifiers=[
             "Development Status :: 7 - Inactive",
             "Intended Audience :: Developers",
             "Topic :: Software Development",
             "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             "License :: OSI Approved :: MIT License",
             "License :: OSI Approved :: Apache Software License",
             "Programming Language :: Python",
             "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
-        test_suite=package_name + ".tests",
-        packages=find_packages(),
-        package_dir={package_name: package_name},
-        install_requires=["fabric2"],
-        data_files=[
-            (_data_install_dir(), list(map(_data_join, listdir(_data_join()))))
-        ],
+        url="https://github.com/offscale/{}".format(package_name),
     )
+
+
+def setup_py_main():
+    """Calls main if `__name__ == '__main__'`"""
+    if __name__ == "__main__":
+        main()
+
+
+setup_py_main()
