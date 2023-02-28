7bc0b11c918ff1b6f0d271fc9ebbb5fbd5db0022
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-02-27 23:52:55 -0500

    [offregister_bootstrap/fedora.py] Fedora-based distributions support ; [LICENSE-{APACHE,MIT}] Update copyright year range ; [offregister_bootstrap/debian.py] Conform to new Fabric spec

--- offregister_bootstrap/fedora.py
+++ offregister_bootstrap/fedora.py
@@ -1,6 +1,3 @@
-from offregister_fab_utils.apt import apt_depends
-
-
 def set_hostname0(c, cache, *args, **kwargs):
     """
     Set the hostname to original hostname sans everything following first `.`
@@ -14,11 +11,9 @@ def set_hostname0(c, cache, *args, **kwargs):
     :return: {"os_version": os_version}
     :rtype: ```dict```
     """
-    apt_depends(c, "base-files")
     hostname = c.run("hostname", hide=True)
     first_dot = hostname.stdout.find(".")
     if first_dot != -1:
-        apt_depends(c, "systemd")
         c.sudo("hostnamectl set-hostname {}".format(hostname.stdout[:first_dot]))
     return {"os_version": cache["os_version"]}
 
--- offregister_bootstrap/ubuntu.py
+++ offregister_bootstrap/ubuntu.py
@@ -7,7 +7,7 @@ from offregister_fab_utils.ubuntu.hostname import set_hostname
 
 @require_os_version(16.04, op=gte)
 def set_hostname0(c, cache, *args, **kwargs):
-    hostname = c.run("hostname", hide=True)
+    hostname = c.run("hostname", hide=True).stdout
     if "." in hostname:
         set_hostname(c, hostname.partition(".")[0])
     return {"os_version": cache["os_version"]}
